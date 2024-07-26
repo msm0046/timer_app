9b5a0b0c5dd00c813ebcbaec5538ad7e14d5279a
Masumi Komatsu <msm0046@icloud.com>
2024-07-27 06:33:53 +0900

    Commit message

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.log("Hello World!")
+
+document.addEventListener("DOMContentLoaded", (_e) => {
+  // 「開始」ボタンが押されたら、 js-coutdownScreen のみを表示する
+  const startButton = document.querySelector(".js-startButton")
+
+  startButton.addEventListener("click", (_e) => {
+    // クリックされたら... (何かする)
+
+    // js-countdownScreen のみを表示する
+    // → js-startScreen, js-finishScreen を非表示にする (等価)
+
+    ['.js-startScreen', '.js-finishScreen'].forEach((elementTarget) => {
+      let element = document.querySelector(elementTarget)
+      // element に対して クラスを付与する (非表示用クラス)
+      element.classList.add("hidden-screen")
+    })
+  })
+
+  // TODO: クリックされたら、 js-startScreen のみを表示する
+})
